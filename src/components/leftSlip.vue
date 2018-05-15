7cfbee66df2e8507efca39bee626278bcd7703d8
eddie <mengxiangyu94@163.com>
2018-05-15 10:03:57 +0800

    first commit

---
+++ src/components/leftSlip.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="left-slip-wrap">
+    <solt></solt>
+    <div class="cancel">取消</div>
+    <div class="delete-confirm">删除</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LeftSlip'
+}
+</script>
+
+<style>
+
+</style>
+
